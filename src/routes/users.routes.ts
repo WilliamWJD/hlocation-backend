00bba12694c0816dcf1b24b1be0bcd5a84f41e71
williamwjd <william.jose.wjd@gmail.com>
2020-09-04 16:43:42 -0300

    criando usuário com senha criptografada

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,27 @@
 import { Router } from 'express';
 
+import CreateUserService from '../services/CreateUserService';
+
 const routes = Router();
 
-routes.post('/', (req, res) => {
+routes.post('/', async (req, res) => {
   const { name, email, password } = req.body;
-  return res.json({ name, email, password });
+
+  try {
+    const createUserService = new CreateUserService();
+
+    const user = await createUserService.execute({
+      name,
+      email,
+      password,
+    });
+
+    delete user.password;
+
+    return res.json(user);
+  } catch (err) {
+    return res.status(401).json({ error: err.message });
+  }
 });
 
 export default routes;
--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,3 +1,8 @@
+import { getRepository } from 'typeorm';
+import bcrypt from 'bcryptjs';
+
+import User from '../models/User';
+
 interface Request {
   name: string;
   email: string;
@@ -5,7 +10,25 @@ interface Request {
 }
 
 class CreateUserService {
-  public async execute({ name, email, password }: Request): Promise<void> { }
+  public async execute({ name, email, password }: Request): Promise<User> {
+    const userRepository = getRepository(User);
+
+    const checkUser = await userRepository.findOne({
+      where: { email },
+    });
+
+    if (checkUser) {
+      throw Error('Email already exists');
+    }
+
+    const passwordHash = await bcrypt.hash(password, 8);
+
+    const user = userRepository.create({ name, email, password: passwordHash });
+
+    await userRepository.save(user);
+
+    return user;
+  }
 }
 
 export default CreateUserService;
