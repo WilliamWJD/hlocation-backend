970dac05ab46823cc6771113dc4a1458e202a955
WilliamWJD <william.jose.wjd@gmail.com>
2020-09-09 06:30:37 -0300

    migration properties

---
+++ src/database/migrations/1599643149732-CreateProperties.ts
@@ -0,0 +1,63 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class CreateProperties1599643149732
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'properties',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+          },
+          {
+            name: 'number',
+            type: 'int',
+          },
+          {
+            name: 'rent_money',
+            type: 'decimal',
+          },
+          {
+            name: 'user_id',
+            type: 'uuid',
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'properties',
+      new TableForeignKey({
+        name: 'PropertieUser',
+        columnNames: ['user_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('properties', 'PropertieUser');
+    await queryRunner.dropTable('properties');
+  }
+}
