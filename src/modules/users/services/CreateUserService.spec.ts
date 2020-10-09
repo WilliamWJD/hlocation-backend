db096b6a81593d0fa373fc32ed1d6248320e1b08
William <william.jose.wjd@gmail.com>
2020-10-09 19:29:11 -0300

    criando arquivo de teste para criaçao de um uusuario

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import CreateUserServices from './CreateUserService';
 import FakeUserRepository from '../repositories/fakes/FakeUserRepository';
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
@@ -16,12 +17,29 @@ describe('CreateUser', () => {
       fakeHashProvider,
     );
   });
+
   it('should be able to create a new user', async () => {
     const user = await createUserServices.execute({
       name: 'fulano',
       email: 'fulano@email.com.br',
       password: '123456',
     });
-    expect('user').toHaveProperty(user.id);
+    expect(user).toHaveProperty('id');
+  });
+
+  it('should not be able to create a user with an email registered', async () => {
+    await createUserServices.execute({
+      name: 'fulano',
+      email: 'fulano@email.com.br',
+      password: '123456',
+    });
+
+    await expect(
+      createUserServices.execute({
+        name: 'fulano2',
+        email: 'fulano@email.com.br',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
