95e347c7c963918661578a88e70d8d89064c675c
William <william.jose.wjd@gmail.com>
2020-10-21 17:22:54 -0300

    teste para nao permitir a criaçao de um inquilino com um cpf ja existente

--- src/modules/tenents/services/CreateTenent.spec.ts
+++ src/modules/tenents/services/CreateTenent.spec.ts
@@ -1,10 +1,11 @@
+import AppError from '@shared/errors/AppError';
+
 import CreateUserService from '@modules/users/services/CreateUserService';
 import FakeUserRepository from '@modules/users/repositories/fakes/FakeUserRepository';
 import FakeHashProvider from '@modules/users/providers/HashProvider/fakes/FakeHashProvider';
 
 import CreateTenentService from './CreateTenent';
 import FakeTenentRepository from '../repositories/fakes/FakeTenentRepository';
-import Tenent from '../infra/typeorm/entities/Tenents';
 
 let fakeTenentRepository: FakeTenentRepository;
 let createTenentService: CreateTenentService;
@@ -47,5 +48,41 @@ describe('CreateTenant', () => {
     });
 
     expect(tenant).toHaveProperty('id');
+  });
+
+  it('should not be able to create a user with cpf registered', async () => {
+    const user = await createUserService.execute({
+      name: 'Fulano',
+      email: 'fulano@email.com.br',
+      password: '123456',
+    });
+
+    await createTenentService.execute({
+      name: 'Tenant',
+      email: 'tenant@email.com.br',
+      cpf: '123456',
+      rg: '123456',
+      genre: 'M',
+      marital_status: 'Casado',
+      phone1: '123456',
+      phone2: '123456',
+      profession: 'dev',
+      user_id: user.id,
+    });
+
+    await expect(
+      createTenentService.execute({
+        name: 'Tenant',
+        email: 'tenant@email.com.br',
+        cpf: '123456',
+        rg: '123456',
+        genre: 'M',
+        marital_status: 'Casado',
+        phone1: '123456',
+        phone2: '123456',
+        profession: 'dev',
+        user_id: user.id,
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
