56d80a600ebdc4e92bee08db443e36a7d867c85c
William <william.jose.wjd@gmail.com>
2020-10-21 16:20:43 -0300

    finalizando testes para atualizaçao de um avatar

--- src/modules/users/services/UpdateUserAvatarService.spec.ts
+++ src/modules/users/services/UpdateUserAvatarService.spec.ts
@@ -1,5 +1,6 @@
 // import AppError from '@shared/errors/AppError';
 import FakeStorageProvider from '@shared/container/providers/StorageProvider/fakes/FakeStorageProvider';
+import AppError from '@shared/errors/AppError';
 import FakeUserRepository from '../repositories/fakes/FakeUserRepository';
 import UpdateUserAvatarService from './UpdateUserAvatarService';
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
@@ -16,6 +17,7 @@ describe('UpdateUser', () => {
   beforeEach(() => {
     fakeUserRepository = new FakeUserRepository();
     fakeHashProvider = new FakeHashProvider();
+    fakeStorageProvider = new FakeStorageProvider();
 
     createUserService = new CreateUserService(
       fakeUserRepository,
@@ -41,5 +43,37 @@ describe('UpdateUser', () => {
     });
 
     expect(user.avatar).toBe('avatar.jpg');
+  });
+
+  it('should not be able to update the avatar of an unauthenticated user', async () => {
+    await expect(
+      updateUserAvatarService.execute({
+        user_id: 'user_not_existing',
+        avatar: 'avatar.jpg',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should be able to delete old avatar when updating a new user', async () => {
+    const deleteFile = jest.spyOn(fakeStorageProvider, 'deleteFile');
+
+    const user = await createUserService.execute({
+      name: 'fulano',
+      email: 'fulano@fulano.com.br',
+      password: '123456',
+    });
+
+    await updateUserAvatarService.execute({
+      user_id: user.id,
+      avatar: 'avatar.jpg',
+    });
+
+    await updateUserAvatarService.execute({
+      user_id: user.id,
+      avatar: 'avatar2.jpg',
+    });
+
+    expect(deleteFile).toBeCalledWith('avatar.jpg');
+    expect(user.avatar).toBe('avatar2.jpg');
   });
 });
